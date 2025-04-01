56c3cc725586e8ced71f2ea2f7ca14517100a085
Jacob Korsgaard <felizk@felizk.dk>
2025-04-01 20:08:45 +0200

    Added a link to the article

--- build_and_deploy.bat
+++ build_and_deploy.bat
@@ -1,3 +1,4 @@
 @echo off
+@REM https://dev.to/felizk/remote-deploy-with-docker-compose-to-unraid-1dai
 docker-compose --context unraid -f docker-compose.yml down --rmi local
 docker-compose --context unraid -f docker-compose.yml up --build
